2338d0f601cfd8bd883ca6fb90127f276107e58c
MuhammadYounas124 <younasraja300@gmail.com>
2024-11-24 01:49:23 +0300

    new form validation

--- registration-form-validation/src/App.tsx
+++ registration-form-validation/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <div><h1>Validation B1</h1></div>
+        <div><h1>new form validation</h1></div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
