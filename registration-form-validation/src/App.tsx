957fb65e279ee5bc93791e65a6ee019ad7b128d4
MuhammadYounas124 <younasraja300@gmail.com>
2024-11-25 03:18:10 +0300

    validation code

--- registration-form-validation/src/App.tsx
+++ registration-form-validation/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "../public/dist/css/adminlte.min.css"; // Ensure the CSS file is correctly linked
 import { useNavigate } from "react-router-dom";
 
+
 const RegistrationForm = () => {
   const [formData, setFormData] = useState({
     firstName: "",
